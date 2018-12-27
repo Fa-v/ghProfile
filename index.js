f8b2d88bf332ce3daafdab92461903e73bb889e8
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2018-12-27 14:53:31 +0100

    Fetching repos and commits to find email

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
   const searchBox = document.forms['searchBar'];
   const searchInput = document.getElementById('submit-input');
   const container = document.querySelector('.container');
+  let user;
 
   /**
    * Fetches user's public profile from GitHub 'users' api
@@ -23,6 +24,44 @@
         }
       })
       .then(data => profile.renderProfile(data))
+      .catch(error => console.log(error));
+  };
+
+  /**
+   * Fetches public repositories a specific user in from GitHub api
+   * @param {String} url
+   * @returns {Void}
+   * @this Window
+   */
+  profile.fetchRepos = url => {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error fetching user data');
+        }
+      })
+      .then(data => profile.searchForCommitsUrl(data))
+      .catch(error => console.log(error));
+  };
+
+  /**
+   * Fetches commits in public repositories from a specific user GitHub api
+   * @param {String} url
+   * @returns {Void}
+   * @this Window
+   */
+  profile.fetchCommits = url => {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error fetching user data');
+        }
+      })
+      .then(data => profile.searchForEmail(data))
       .catch(error => console.log(error));
   };
 
@@ -49,10 +88,30 @@
   profile.setUpEventListener = () => {
     searchBox.addEventListener('submit', event => {
       event.preventDefault();
-      let user = searchInput.value;
+      user = searchInput.value;
       let url = profile.composeUrl(user);
       profile.getUserData(url);
     });
+  };
+
+  profile.searchForCommitsUrl = data => {
+    let commitUrl = data.map(repo => {
+      let url = repo.commits_url;
+      return (url = url.slice(0, -6));
+    });
+    profile.fetchCommits(commitUrl[0]);
+  };
+
+  profile.searchForEmail = data => {
+    const emailNode = document.getElementById('email');
+    let email;
+    data.map(item => {
+      email = item.commit.author.email;
+      console.log(email);
+      return email;
+    });
+    let text = `Email: ${email}`;
+    emailNode.innerText = text;
   };
 
   /**
@@ -64,11 +123,18 @@
   profile.renderProfile = data => {
     const card = document.querySelector('.card');
     card.innerHTML = '';
+    if (data.email === null) {
+      const clientID = '';
+      const secret = '';
+      const baseUrl = 'https://api.github.com/users/';
+      const composedUrl = `${baseUrl}${user}/repos?client_id=${clientID}&client_secret=${secret}`;
+      profile.fetchRepos(composedUrl);
+    }
 
     let template = `
       <h4>Name: ${data.name}</h4>
       <p>GitHub user: ${data.login}</p>
-      <p>Email: ${data.email}</p>
+      <p id="email">Email: ${data.email}</p>
       <p>Bio: ${data.bio}</p>
       <p>Repos: ${data.public_repos}</p>
       <p>Followers: ${data.followers}</p>
